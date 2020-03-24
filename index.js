913ae8c57394822775f5ea66f57763b2c7d1d04e
Steven Luck <steve.luck@gmail.com>
2020-03-24 20:44:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function placeOrder(cardNumber) {
     return("Sorry, we don't have a credit card on file for you.")
   } else {
     let tot=total()
-    response= `Your total cost is $${tot}, which will be charged to the card ${cardNumber}`;
+    let response= `Your total cost is $${tot}, which will be charged to the card ${cardNumber}`;
     cart=[];
     return(response);
     
