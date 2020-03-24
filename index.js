50a71f8e941c4d115e901cc9292055dd99559052
Steven Luck <steve.luck@gmail.com>
2020-03-24 19:14:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,10 +21,11 @@ function viewCart() {
     return(`Your shopping cart is empty.`);
   } else {
     let response=`In your cart, you have `;
-    response=`${response}${cart[0].itemName} at `
+    response=`${response}${cart[0].itemName} at ${cart[0].itemPrice}`;
     for(let i=0;i<cart.length;i++) {
       
     }
+    response=`${response}.`
     return(response)
   }
 }
