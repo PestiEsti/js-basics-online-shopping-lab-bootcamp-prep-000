fba728b7447e77e2e1c0c30aac95b02e13815195
Steven Luck <steve.luck@gmail.com>
2020-03-24 20:30:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -44,7 +44,11 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
-  for 
+  for (let i=0;i<cart.length;i++) {
+    if cart[i].itemName=item {
+      cart[i].
+    }
+  } 
 }
 
 function placeOrder(cardNumber) {
